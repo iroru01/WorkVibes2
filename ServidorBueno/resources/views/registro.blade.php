b202cfa695672c3109015ddf6c4e58afba19bfa6
DESKTOP-OFJNUE7\Habbit <thebarrient97@hotmail.com>
2024-02-26 11:15:15 +0100

    Funcional_lunes

--- ServidorBueno/resources/views/registro.blade.php
+++ ServidorBueno/resources/views/registro.blade.php
@@ -13,7 +13,7 @@
     <link rel="stylesheet" href="fuentes_awesome/all.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
-    <script src="js/Registro.js"></script>
+    {{-- <script src="js/Registro.js"></script> --}}
  
 </head>
 <body>
@@ -33,96 +33,43 @@
 <div class="container-fluid">
     <div class="row align-center">
         <div class="col-12 col-md-5 offset-md-2">
-            <h5>Introduce tus datos personales</h5>
-            <form method="POST" action="{{ route('usuarios.store') }}">
-                @csrf
-                <input type="text" placeholder="Nombre de usuario"value="" class="form-control col-8" id="nombre_usuario" name="nombre_usuario" required>
-                <div class="error-message" id="nombre_usuario-error"></div>
-                <input type="text" placeholder="Nombre"value=""  class="form-control col-8" id="nombre" name="nombre" required>
-                <div class="error-message" id="nombre-error"></div>
-                <input type="text" placeholder="Apellido" value="" class="form-control col-8" id="apellido" name="apellido">
-                <div class="error-message" id="apellido-error"></div>
-                <input type="text" placeholder="DNI" value="" class="form-control col-8" id="dni" name="dni">
-                <div class="error-message" id="dni-error"></div>
-                <input type="text" placeholder="Teléfono" value="" class="form-control col-8" id="telefono" name="telefono">
-                <div class="error-message" id="telefono-error"></div>
-                <input type="text" placeholder="Dirección" value="" class="form-control col-8" id="direccion" name="direccion">
-                <div class="error-message" id="direccion-error"></div>
-                <input type="password" placeholder="Contraseña" value="" class="form-control col-8" id="contraseña" name="contraseña">
-                <div class="error-message" id="contraseña-error"></div>
-                <input type="password" placeholder="Confirmar contraseña" class="form-control col-8" id="confirma_contraseña" name="confirma_contraseña">
-                <div class="error-message" id="confirma_contraseña-error"></div>
+        <h5>Introduce tus datos personales</h5>
+        
+        <form id="form1" method="POST" action="{{ route('guardar.usuario') }}">
+            @csrf
+            <input type="text" name="nombre_user" class="form-control col-8" placeholder="Nombre de Usuario:" id="nombre_user" required>
+            
+            <input type="text" class="form-control col-8" name="dni" id="dni" placeholder="DNI" required>
 
-                <!-- QUITAR HREF -->
-                <div class="row justify-content-center"> <!-- Añadido para centrar el botón -->
-                    <button type="submit" class="form-control col-6" id="btn_next" name="button" required>Siguiente</button>
-                </div>
-            </form>
+            <input type="text" class="form-control col-8" name="nombre" id="nombre" placeholder="Nombre:" required>
+
+            <input type="text" name="apellidos" class="form-control col-8" placeholder="Apellidos:" id="apellido" required>
+
+            <input type="text" name="telefono" class="form-control col-8" placeholder="Teléfono:" id="telefono" required>
+
+            <input type="text" name="direccion" class="form-control col-8" id="direccion" placeholder="Dirección:" required>
+
+            <input type="password" name="contraseña" class="form-control col-8" id="contraseña" placeholder="Contraseña:" required>
         </div>
-
-        <form method="POST" action="{{ route('guardar.usuario') }}">
-        @csrf
-
-        <label for="nombre_user">Nombre de Usuario:</label>
-        <input type="text" name="nombre_user" required><br>
-
-        <label for="dni">DNI:</label>
-        <input type="text" name="dni" required><br>
-
-        <label for="nombre">Nombre:</label>
-        <input type="text" name="nombre" required><br>
-
-        <label for="apellidos">Apellidos:</label>
-        <input type="text" name="apellidos" required><br>
-
-        <label for="telefono">Teléfono:</label>
-        <input type="text" name="telefono" required><br>
-
-        <label for="direccion">Dirección:</label>
-        <input type="text" name="direccion" required><br>
-
-        <label for="contraseña">Contraseña:</label>
-        <input type="password" name="contraseña" required><br>
-
-        <label for="puesto">Puesto:</label>
-        <input type="text" name="puesto" required><br>
-
-        <label for="incorporacion">Fecha de Incorporación:</label>
-        <input type="date" name="incorporacion" required><br>
-
-        <label for="id_departamento">ID del Departamento:</label>
-        <input type="text" name="id_departamento"><br>
-
-        <label for="id_evento">ID del Evento:</label>
-        <input type="text" name="id_evento"><br>
-
-        <label for="estado">Estado:</label>
-        <input type="text" name="estado" required><br>
-
-        <button type="submit">Guardar Usuario</button>
         <div class="row" id="section_encargado">
             <div class="col-12">
-                <h4>¿Eres Jefe o Empleado</h4>
-                <div class="row justify-content-center">
-                    <div class="col-auto mr-2">
-                        <input type="radio" class="btn_radio" name="jefe" value="puesto"><span class="span_text">Jefe</span>
-                    </div>
-                    <div class="col-auto">
-                        <input type="radio" class="btn_radio" name="empleado" value="puesto"><span class="span_text">Empleado</span>
-                    </div>
-                </div>
-                <div class="error-message" id="jefe-error"></div>
+                <h4>¿Eres jefe o empleado?</h4>
+                <label for="puesto">Puesto:</label>
+                <select class="form-control" name="puesto" id="puesto">
+                    <option value="Jefe">Jefe</option>
+                    <option value="Empleado">Empleado</option>
+                </select>
                 <br>
-                <h4>¿Cuándo te incorporaste a la empresa?</h4>
-                <div class="row justify-content-center">
-                    <input type="date" name="fecha" class="form-control col-8" id="fecha">
-                    <div class="error-message" id="fecha-error"></div>
-                </div>
-                
+                <label for="incorporacion">Fecha de Incorporación:</label>
+                <input class="form-control" type="date" name="incorporacion" id="incorporacion" required>
+        
+                <button id="btn_next" type="submit" class="form-control col-8">Guardar Usuario</button>
             </div>
         </div>
+
     </div>
 </div>
 
 </body>
 </html>
+{{-- --}}
